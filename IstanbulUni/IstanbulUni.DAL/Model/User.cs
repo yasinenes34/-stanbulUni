2224225d4b8f9ad28f6bf5e5f5611ad17cc78feb
yasin yıldırım <yasinnazmi1@gmail.com>
2022-06-27 17:08:50 +0300

    telerik-kurulum

--- IstanbulUni/IstanbulUni.DAL/Model/User.cs
+++ IstanbulUni/IstanbulUni.DAL/Model/User.cs
@@ -18,6 +18,8 @@ namespace IstanbulUni.DAL.Model
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
 
     }
 }
--- IstanbulUni/IstanbulUni.WebUI/App_Start/RouteConfig.cs
+++ IstanbulUni/IstanbulUni.WebUI/App_Start/RouteConfig.cs
@@ -13,11 +13,32 @@ namespace IstanbulUni.WebUI
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                   name: "Anasayfa",
+                   url: "",
+                   defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               );
+            routes.MapRoute(
+                  name: "Anasayfa2",
+                  url: "anasayfa",
+                  defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              );
+            routes.MapRoute(
+                   name: "Register",
+                   url: "register",
+                   defaults: new { controller = "Register", action = "Register", id = UrlParameter.Optional }
+               );
+            routes.MapRoute(
+                  name: "Login",
+                  url: "login",
+                  defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+              );
+
         }
     }
 }
